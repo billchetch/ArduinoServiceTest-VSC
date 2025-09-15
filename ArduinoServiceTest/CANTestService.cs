eafdf3bd17ae4159ce4fcbbff26e6fbec3957d2c
bill@chetch.net <bill@chetch.net>
2025-09-15 16:44:47 +0800

    asdf

--- ArduinoServiceTest/CANTestService.cs
+++ ArduinoServiceTest/CANTestService.cs
@@ -16,6 +16,8 @@ public class CANTestService : CANBusService<CANTestService>
 
     public const String COMMAND_RESUME = "resume";
 
+    public const String COMMAND_SHOW_ANOMALIES = "show-anomalies";
+
     public const int REMOTE_NODES = 3;
 
     public class ReportData
@@ -122,7 +124,7 @@ public class CANTestService : CANBusService<CANTestService>
             sb.AppendLine();
             sb.AppendFormat(" - MinIdle: Node {0} -> {1}", MinIdleNode, MinIdle);
             sb.AppendLine();
-            sb.AppendFormat(" - Errors TX/RX/MSG: {0} {1} {2}", TXErrorCount, RXErrorCount, MsgErrorCount);
+            sb.AppendFormat(" - Errors TX/RX/MSG/SF: {0} {1} {2} {3}", TXErrorCount, RXErrorCount, MsgErrorCount, SendFails);
             sb.AppendLine();
             sb.AppendFormat(" - DiffEqualRepeatError: {0}", DiffEqualRepeatErrorCount);
             sb.AppendLine();
@@ -153,11 +155,107 @@ public class CANTestService : CANBusService<CANTestService>
         }
     }
 
+    public class MessageData
+    {
+        public enum Status
+        {
+            NOT_SET = 0,
+            OK,
+            GREATER_THAN,
+            EQUAL_REPEAT,
+            EQUAL_RESENT,
+            EQUAL_UNKNOWN,
+            LESS_THAN
+        }
+
+        public Status ReadStatus = Status.NOT_SET;
+
+        public bool IsOK => ReadStatus == Status.OK;
+
+        public byte NodeID = 0;
+        public UInt32 Value = 0;
+        public UInt32 Time = 0; //time in ms that it was sent
+
+        public UInt32 NewValue = 0;
+        public UInt32 NewTime = 0; //time in ms that it was sent
+
+        public MessageData(byte nodeID)
+        {
+            NodeID = nodeID;
+        }
+
+        public MessageData(MessageData md)
+        {
+            NodeID = md.NodeID;
+            Value = md.Value;
+            Time = md.Time;
+            NewValue = md.NewValue;
+            NewTime = md.NewTime;
+            ReadStatus = md.ReadStatus;
+        }
+
+        public void Read(ArduinoMessage msg)
+        {
+            ReadStatus = Status.NOT_SET;
+
+            NewValue = msg.Get<UInt32>(0);
+            NewTime = msg.Get<UInt32>(1);
+
+            if (NewValue == Value + 1)
+            {
+                Value = NewValue;
+                Time = NewTime;
+                ReadStatus = Status.OK;
+            }
+            else
+            {
+                if (NewValue > Value)
+                {
+                    ReadStatus = Status.GREATER_THAN;
+                }
+                else if (NewValue == Value)
+                {
+                    if (NewTime == Time)
+                    {
+                        ReadStatus = Status.EQUAL_REPEAT; //repeat or reflection or something
+                    }
+                    else if (NewTime > Time)
+                    {
+                        ReadStatus = Status.EQUAL_RESENT; //a later time sent so this is indiciative of a resend
+                    }
+                    else
+                    {
+                        ReadStatus = Status.EQUAL_UNKNOWN; //Time is less ... weird
+                    }
+                }
+                else
+                {
+                    ReadStatus = Status.LESS_THAN;
+                }
+            }
+        }
+        public override String ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Message Data for Node {0} status {1}", NodeID, ReadStatus);
+            sb.AppendLine();
+            sb.AppendFormat(" - Value: {0} {1}", Value, NewValue);
+            sb.AppendLine();
+            sb.AppendFormat(" - Time: {0} {1}", Time, NewTime);
+            sb.AppendLine();
+            return sb.ToString();
+        }
+    }
+
     public CANBusMonitor BusMonitor { get; } //For easy access
 
     RingBuffer<ReportData> log = new RingBuffer<ReportData>(100, true);
     Dictionary<byte, ReportData> reportData = new Dictionary<byte, ReportData>();
 
+    Dictionary<byte, MessageData> recvMessageData = new Dictionary<byte, MessageData>();
+
+    List<MessageData> anomalies = new List<MessageData>();
+    
     public CANTestService(ILogger<CANTestService> Logger) : base(Logger)
     {
         BusMonitor = new CANBusMonitor(REMOTE_NODES);
@@ -191,6 +289,20 @@ public class CANTestService : CANBusService<CANTestService>
                     Console.WriteLine(e.Message);
                 }
             }
+            else if (msg.Type == MessageType.DATA)
+            {
+                if (!recvMessageData.ContainsKey(eargs.NodeID))
+                {
+                    recvMessageData[eargs.NodeID] = new MessageData(eargs.NodeID);
+                }
+                recvMessageData[eargs.NodeID].Read(msg);
+
+                Console.WriteLine(recvMessageData[eargs.NodeID].ToString());
+                if (!recvMessageData[eargs.NodeID].IsOK)
+                {
+                    anomalies.Add(new MessageData(recvMessageData[eargs.NodeID]));
+                }
+            }
         };
 
         //Add the bus monitor to the service
@@ -209,21 +321,37 @@ public class CANTestService : CANBusService<CANTestService>
     protected override void AddCommands()
     {
         AddCommand(COMMAND_SHOW_LOG, "Show <n?> items from log, if no number is given will show last item");
+        AddCommand(COMMAND_SHOW_ANOMALIES, "Show <n?> last message data anomalises");
         AddCommand(COMMAND_PAUSE, "Pause the current test");
         AddCommand(COMMAND_RESUME, "Resume the current test");
+
         base.AddCommands();
     }
 
     protected override bool HandleCommandReceived(ServiceCommand command, List<object> arguments, Message response)
     {
+        int n;
+        int c;
+
         switch (command.Command)
         {
             case COMMAND_SHOW_LOG:
-                int n = arguments.Count > 0 ? System.Convert.ToInt32(arguments[0].ToString()) : 1;
-                int c = 0;
+                n = arguments.Count > 0 ? System.Convert.ToInt32(arguments[0].ToString()) : 1;
+                c = 0;
                 foreach (var rd in log)
                 {
                     response.AddValue("E" + c, rd.ToString());
+                    c++;
+                    if (c == n) break;
+                }
+                return true;
+
+            case COMMAND_SHOW_ANOMALIES:
+                n = arguments.Count > 0 ? System.Convert.ToInt32(arguments[0].ToString()) : 1;
+                c = 0;
+                foreach (var ad in COMMAND_SHOW_ANOMALIES)
+                {
+                    response.AddValue("E" + c, ad.ToString());
                     c++;
                     if (c == n) break;
                 }
@@ -246,6 +374,7 @@ public class CANTestService : CANBusService<CANTestService>
     {
         StatusDetails["MasterNode"] = "Todo";
         StatusDetails["Log"] = String.Format("Contains {0} entries", log.Count);
+        StatusDetails["Anomalies"] = String.Format("Contains {0} items", anomalies.Count);
         base.PopulateStatusResponse(response);
     }
     #endregion
